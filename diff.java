43f135dee25eabd630e2d8193573212825f0f1d7
arijit203 <99786400+arijit203@users.noreply.github.com>
2023-06-02 16:04:27 +0530

    Update diff.java

--- diff.java
+++ diff.java
@@ -2,7 +2,8 @@ class diff{
 	public static void main(){
 		int a = 2;
 		int b = 3;
-		System.out.println("Ki gawd bhai tui 3rd question ta kore dili.Ki gawd tui #allgawd "+(a-b));
+		System.out.println("Ki gawd bhai tui 3rd question ta kore dili.Ki gawd tui #allgawd "+(a-b));\
+		System.out.println("HIIIIIII !!! I am Gawd");
 
 	}
 }
